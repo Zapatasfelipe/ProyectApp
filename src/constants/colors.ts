0481a43e82bf101bbf1e937cbd5fd8b627fe6140
fzapatso <fzapatso@emeal.nttdata.com>
2024-10-23 18:34:01 -0300

    colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+    aliceBlue: '#e3f2fdff',
+    uranianBlue: '#bbdefbff',
+    lightSkyBlue: '#90caf9ff',
+    argentinianBlue: '#64b5f6ff',
+    argentinianBlue2: '#42a5f5ff',
+    dodgerBlue: '#2196f3ff',
+    bleuDeFrance: '#1e88e5ff',
+    frenchBlue: '#1976d2ff',
+    greenBlue: '#1565c0ff',
+    cobaltBlue: '#0d47a1ff',
+};
