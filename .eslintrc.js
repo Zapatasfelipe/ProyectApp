4fc46c768d83bc23e3c0a2ec34be7a40e4b5388f
fzapatso <fzapatso@emeal.nttdata.com>
2024-10-23 18:04:37 -0300

    eslint prettierc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+      indent: ['warn', 4, { SwitchCase: 1 }],
+      'max-len': ['warn', { code: 150, ignoreComments: true, ignoreUrls: true }],
+      eqeqeq: ['error', 'always'],
+      quotes: ['warn', 'single'],
+      'no-unused-vars': 'warn',
+      'max-depth': ['error', 4],
+      'max-lines': ['error', { max: 500, skipBlankLines: true, skipComments: true }],
+      'no-inline-comments': 'error',
+      'no-nested-ternary': 'warn',
+      'no-undefined': 'warn',
+      'no-var': 'warn',
+      'no-console': 'warn',
+      'default-case': 'warn',
+      'max-params': ['warn', 7],
+      'max-lines-per-function': ['warn', { max: 500, skipBlankLines: true, skipComments: true }],
+  },
 };
+/*
+indent:que indentacion se usa ( 2 espacios)
+max-len:largo maximo de una linea, ignorando comentairos y urls
+eqeqeq:que se use triple = para comparar valores
+quotes: las comillas deben ser simples
+no-unused-vars: si hay variables declaradas, pero no son usadas
+max-depth: permiite hasta 3 anidaciones, 4 genera error
+max-lines: maxima cantidad de lineas por archivo
+no-inline-comments: para evitar comentario en la linea, que puede no verse
+no-ternary: no usar ternario como warning
+no-undefned: no usar el undefined
+no-var: no usar var para definir variables
+no-bitwise: no usar definision de variables & o > o |
+no-console: que los console. algo sean marcados como warning
+no-unrechable-loop: evita o marca si hay cdigo que no se ejecuta.
+no-useless-assgnment: asignacion de variables que despues cambian.
+camelcase: se setea para que las variables y funciones cumplan
+default-case: para que en los switch siempre exista un default
+max-params: definiir maximo de parametros en funciones
+max-lines-per-function:maximas lineas por funcion
+no-eq-null: no comparar con null
+no-nested-ternary: para no naidar ternarios
+*/
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,26 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
+  tabWidth: 4,
+  useTabs: false,
+  printWidth: 100
 };
+/*
+printWidth: el largo de linea que el formateador va a dejar como valido ( y formateara a ese valor )
+tabWidth: cantidad de espacios que incerta al apretar el "Tabulador"
+semicolons: que ponga o no ; al final de cada linea (por defecto = true)
+useTabs: indente lineas con "tabs"
+singleQuote: coloca comas simples en vez de las dobles
+trailingComma: pide o coloca comas a todos los elementos de jsons o arrays
+bracketSpacing: pone o pide un espacio frente a las llaves { }
+bracketSameLine: pone el fin de los tag <> > en la misma linea que finaliza, o abajo
+arrowParens: pide que las funciones flecha; lleven o no los parentecis ()
+endOfLine: detrmina cual es el caracter especificocon la que el archivo termina
+singleAttributePerLine: para los componentes visuales ( html, vue, jsx ) determina poner los atributos, cada uno en una linea o no
+jsxSingleQuote: que se puedan usar sigleQuotes ' o doubleQuotes " en los archivos JSX
+requirePragma: sirve para activar segun comentario inicial en el archivo
+*/
